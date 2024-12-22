704802f1daba1a8ccadaa3050508c6dad7efef53
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2024-12-23 00:14:45 +0530

    feat: call the Home component in App.js

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { PaperProvider, Text, Divider } from 'react-native-paper';
+import Home from './components/Home';
 
 export default function App() {
   return (
     <PaperProvider>
       <View style={styles.container}>
-        <Text variant="headlineLarge">Headline Large</Text>
-        <Divider />
-        <Text variant="bodyMedium" style={styles.body}>Its interface. It is responsive, fast and works reliably on both platforms. When building a React component, you have to style each of them yourself, according to the guidelines of the platform you are targeting. This can be overwhelming and non-trivial to do right. Its interface. It is responsive, fast and works reliably on both platforms. When building a React component, you have to style each of them yourself, according to the guidelines of the platform you are targeting. This can be overwhelming and non-trivial to do right. Its interface. It is responsive, fast and works reliably on both platforms. When building a React component, you have to style each of them yourself, according to the guidelines of the platform you are targeting. This can be overwhelming and non-trivial to do right. Its interface. It is responsive, fast and works reliably on both platforms. When building a React component, you have to style each of them yourself, according to the guidelines of the platform you are targeting. This can be overwhelming and non-trivial to do right.
-        </Text>
+        <Home />
         <StatusBar style="auto" />
       </View>
     </PaperProvider>
