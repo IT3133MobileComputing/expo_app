9b9613da46c1f6dc15952a17d92fd6ed91f6751f
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2024-12-23 00:02:17 +0530

    feat: update the code with another text

--- App.js
+++ App.js
@@ -6,7 +6,10 @@ export default function App() {
   return (
     <PaperProvider>
       <View style={styles.container}>
-        <Text varient="headlineLarge">Headline Large</Text>
+        <Text variant="headlineLarge">Headline Large</Text>
+        
+        <Text variant="bodyMedium" style={styles.body}>Its interface. It is responsive, fast and works reliably on both platforms. When building a React component, you have to style each of them yourself, according to the guidelines of the platform you are targeting. This can be overwhelming and non-trivial to do right. Its interface. It is responsive, fast and works reliably on both platforms. When building a React component, you have to style each of them yourself, according to the guidelines of the platform you are targeting. This can be overwhelming and non-trivial to do right. Its interface. It is responsive, fast and works reliably on both platforms. When building a React component, you have to style each of them yourself, according to the guidelines of the platform you are targeting. This can be overwhelming and non-trivial to do right. Its interface. It is responsive, fast and works reliably on both platforms. When building a React component, you have to style each of them yourself, according to the guidelines of the platform you are targeting. This can be overwhelming and non-trivial to do right.
+        </Text>
         <StatusBar style="auto" />
       </View>
     </PaperProvider>
@@ -20,4 +23,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  body: {
+    padding: 5,
+    textAlign: 'justify',
+  }
 });
